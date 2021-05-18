9d36ad9e7c89de3e964bfa33159b6bac6693d38a
Varun Sharma <varunsharma12@outlook.com>
2021-05-18 12:21:43 +0530

    😀 Longest substring with k distinct characters.

---
+++ algorithms/src/sliding_window/longest_substring_with_k_distinct_characters/LongestSubstringWithKDistinctCharacters.java
@@ -0,0 +1,40 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestSubstringWithKDistinctCharacters {
+    
+
+    public static int longestSubstring(String input, int K){
+
+        Map<Character, Integer> charFrequencies = new HashMap<>();
+
+        int windowStart = 0, maximumLength = 0;
+        for (int windowEnd = 0; windowEnd < input.length(); windowEnd++) {
+            char f = input.charAt(windowEnd);
+            charFrequencies.put(f, charFrequencies.getOrDefault(f, 0) + 1);
+            while (charFrequencies.size() > K) {
+                charFrequencies.put(input.charAt(windowStart), charFrequencies.get(input.charAt(windowStart)) - 1);
+                if(charFrequencies.get(input.charAt(windowStart)) <= 0){
+                    charFrequencies.remove(input.charAt(windowStart));
+                }                
+                windowStart++;
+            }
+            
+            maximumLength = Math.max(maximumLength, windowEnd - windowStart + 1);
+        }
+
+        return maximumLength;
+    }
+
+    public static void main(String[] args) {
+      
+        int longestSubstringSize = LongestSubstringWithKDistinctCharacters.longestSubstring("araaci", 2);
+        System.out.println("Longest substring with k distinct characters is " + longestSubstringSize);
+        
+        int longestSubstringSize1 = LongestSubstringWithKDistinctCharacters.longestSubstring("araaci", 1);
+        System.out.println("Longest substring with k distinct characters is " + longestSubstringSize1);
+        
+        int longestSubstringSize2 = LongestSubstringWithKDistinctCharacters.longestSubstring("cbbebi", 3);
+        System.out.println("Longest substring with k distinct characters is " + longestSubstringSize2);
+    }
+}
