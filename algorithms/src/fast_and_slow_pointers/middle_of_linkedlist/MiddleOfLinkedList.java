7ac5f236f59888ffeff0b09b62d852fbfa262ba4
Varun Sharma <varunsharma12@outlook.com>
2021-05-24 14:37:49 +0530

    😀 Middle of linkedlist.

---
+++ algorithms/src/fast_and_slow_pointers/middle_of_linkedlist/MiddleOfLinkedList.java
@@ -0,0 +1,40 @@
+public class MiddleOfLinkedList {
+    
+    public static ListNode findMiddle(ListNode head){
+
+        if (head == null) return null;
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }   
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        System.out.println("Middle Node: " + MiddleOfLinkedList.findMiddle(head).data);
+
+        head.next.next.next.next.next = new ListNode(6);
+        System.out.println("Middle Node: " + MiddleOfLinkedList.findMiddle(head).data);
+
+        head.next.next.next.next.next.next = new ListNode(7);
+        System.out.println("Middle Node: " + MiddleOfLinkedList.findMiddle(head).data);
+    }
+}
+
+class ListNode{
+    int data;
+    ListNode next;
+
+    public ListNode(int data){
+        this.data = data;
+    }
+}
