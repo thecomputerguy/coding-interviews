b1c4d23acc40301dfb6ec7ac8a42596e67c370ca
Varun Sharma <varunsharma12@outlook.com>
2021-05-27 09:50:17 +0530

    😁 Reverse level order traversal.

---
+++ algorithms/src/bfs/reverse_level_order_traversal/ReverseLevelOrderTraversal.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+class TreeNode{
+    int value;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int value){
+        this.value = value;
+    }
+}
+
+public class ReverseLevelOrderTraversal {
+    
+    public static List<List<Integer>> traverse(TreeNode root){
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if (root == null) return result;
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+
+        queue.add(root);
+
+        while(!queue.isEmpty()){
+            int levelSize = queue.size();
+            List<Integer> currentLevel = new ArrayList<Integer>();
+            int i = 0;
+            while (i < levelSize) {
+                TreeNode node = queue.poll();
+
+                currentLevel.add(node.value);
+
+                if (node.left != null) {
+                    queue.add(node.left);                    
+                }
+
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+
+                i++;
+            }
+
+            result.add(0, currentLevel);
+        }
+
+        return result;
+    }
+    public static void main(String[] args) {
+        
+        TreeNode root = new TreeNode(12);
+        root.left = new TreeNode(7);
+        root.right = new TreeNode(1);
+        root.left.left = new TreeNode(9);
+        root.right.left = new TreeNode(10);
+        root.right.right = new TreeNode(5);
+        List<List<Integer>> result = ReverseLevelOrderTraversal.traverse(root);
+        System.out.println("Reverse level order traversal: " + result);
+    }
+}
