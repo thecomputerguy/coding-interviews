d40e3022e4c08d88759ecc17425fc8a0892c76b1
Varun Sharma <varunsharma12@outlook.com>
2021-06-03 12:03:35 +0530

    � Rotation count of Rotated Array.

---
+++ algorithms/src/modified_binary_search/rotation_count_of_rotated_array/RotationCountOfRotatedArray.java
@@ -0,0 +1,32 @@
+class RotationCountOfRotatedArray {
+
+  public static int countRotations(int[] arr) {
+    int start = 0;
+    int end = arr.length - 1;
+
+    while(start <= end){
+        int mid = start + (end - start) / 2;
+
+        if(mid < end && arr[mid] > arr[mid + 1]){
+            return mid + 1;
+        }
+
+        if(mid > start && arr[mid - 1] > arr[mid]){
+            return mid;
+        }
+
+        if(arr[start] < arr[mid]){
+            start = mid + 1;
+        }else{
+            end = mid - 1;
+        }
+    }
+    return 0; 
+  }
+
+  public static void main(String[] args) {
+    System.out.println(RotationCountOfRotatedArray.countRotations(new int[] { 10, 15, 1, 3, 8 }));
+    System.out.println(RotationCountOfRotatedArray.countRotations(new int[] { 4, 5, 7, 9, 10, -1, 2 }));
+    System.out.println(RotationCountOfRotatedArray.countRotations(new int[] { 1, 3, 8, 10 }));
+  }
+}
