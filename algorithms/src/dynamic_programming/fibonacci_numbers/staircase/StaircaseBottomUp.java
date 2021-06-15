8b840e3cb2d4bc05dc385b58565a05b26b4a277b
Varun Sharma <varunsharma12@outlook.com>
2021-06-15 23:35:59 +0530

    😁 Staircase bottom up.

---
+++ algorithms/src/dynamic_programming/fibonacci_numbers/staircase/StaircaseBottomUp.java
@@ -0,0 +1,22 @@
+public class StaircaseBottomUp {
+
+  public int countWays(int n) {
+    if(n == 0) return 1;
+    int dp[] = new int[n + 1];
+    dp[0] = 1;
+    dp[1] = 1;
+    dp[2] = 2;
+    for (int i = 3; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+    }
+    return dp[n];
+  }
+
+  public static void main(String[] args) {
+    StaircaseBottomUp sc = new StaircaseBottomUp();
+    System.out.println(sc.countWays(3));
+    System.out.println(sc.countWays(4));
+    System.out.println(sc.countWays(5));
+  }
+
+}
