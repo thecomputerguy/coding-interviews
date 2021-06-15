e53e39cd02ae6ff70e548334f914cfbeb156f588
Varun Sharma <varunsharma12@outlook.com>
2021-06-16 00:50:14 +0530

    😁 Number factors with memoization

---
+++ algorithms/src/dynamic_programming/fibonacci_numbers/number_factors/NumberFactorsRecursive.java
@@ -0,0 +1,31 @@
+public class NumberFactorsRecursive {
+    
+    public int countWays(int n, Integer[] dp){
+        if(n == 0){
+            return 1;
+        }
+        if(n == 1){
+            return 1;
+        }
+        if(n == 2){
+            return 1;
+        }
+        if(n == 3){
+            return 2;
+        }
+
+        if(dp[n] != null){
+            return dp[n];
+        }
+        
+        dp[n] = countWays(n - 1, dp) + countWays(n - 3, dp) + countWays(n - 4, dp);
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        NumberFactors en = new NumberFactors();
+        System.out.println(en.countWays(4));
+        System.out.println(en.countWays(5));
+        System.out.println(en.countWays(6));
+    }
+}
