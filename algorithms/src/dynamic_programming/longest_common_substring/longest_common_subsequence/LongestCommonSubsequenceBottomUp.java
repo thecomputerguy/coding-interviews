464899c52cfc87f1d19684fb49f66efd511b0a67
Varun Sharma <varunsharma12@outlook.com>
2021-06-23 18:57:39 +0530

    😁 Longest common subsequence bottom up solution.

---
+++ algorithms/src/dynamic_programming/longest_common_substring/longest_common_subsequence/LongestCommonSubsequenceBottomUp.java
@@ -0,0 +1,29 @@
+public class LongestCommonSubsequenceBottomUp {
+    
+    public int findLCSLength(String s1, String s2) {
+        if(s1 == null || s2 == null || s1.length() == 0 || s2.length() == 0){
+            return 0;
+        }
+
+        int[][] dp = new int[s1.length() + 1][s2.length() + 1];
+        int maxLength = 0;
+        for (int i = 1; i <= s1.length(); i++) {
+            for (int j = 1; j <= s2.length(); j++) {
+                if(s1.charAt(i - 1) == s2.charAt(j - 1)){
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                }else{
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+                maxLength = Math.max(dp[i][j], maxLength);
+            }
+        }
+
+        return maxLength;
+    }
+
+    public static void main(String[] args) {   
+        LongestCommonSubsequenceBottomUp lcs = new LongestCommonSubsequenceBottomUp();
+        System.out.println(lcs.findLCSLength("abdca", "cbda"));
+        System.out.println(lcs.findLCSLength("passport", "ppsspt"));
+    }
+}
