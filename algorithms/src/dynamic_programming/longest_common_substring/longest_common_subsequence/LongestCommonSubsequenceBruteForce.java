bbc519dbb88e34bc1701dad6f5e2e323b3dd63f2
Varun Sharma <varunsharma12@outlook.com>
2021-06-23 18:28:22 +0530

    😁 Longest common subsequence brute force solution.

--- algorithms/src/dynamic_programming/longest_common_substring/longest_common_subsequence/LongestCommonSubsequenceBruteForce.java
+++ algorithms/src/dynamic_programming/longest_common_substring/longest_common_subsequence/LongestCommonSubsequenceBruteForce.java
@@ -1,7 +1,31 @@
 public class LongestCommonSubsequenceBruteForce {
     
+    public int findLCSLength(String s1, String s2){
+        if(s1 == null || s2 == null || s1.length() == 0 || s2.length() == 0){
+            return 0;
+        }
+
+        return findLCSLengthRecursive(s1, s2, 0, 0);
+    }
+
+    private int findLCSLengthRecursive(String s1, String s2, int index1, int index2){
+        if(index1 == s1.length() || index2 == s2.length()){
+            return 0;
+        }
+        if(s1.charAt(index1) == s2.charAt(index2)){
+            return 1 + findLCSLengthRecursive(s1, s2, index1 + 1, index2 + 1);
+        }
+
+        int l1 = findLCSLengthRecursive(s1, s2, index1 + 1, index2);
+        int l2 = findLCSLengthRecursive(s1, s2, index1, index2 + 1);
+
+        return Math.max(l1, l2);
+    }
+
     public static void main(String[] args) {
         
-        
+        LongestCommonSubsequenceBruteForce lcs = new LongestCommonSubsequenceBruteForce();
+        System.out.println(lcs.findLCSLength("abdca", "cbda"));
+        System.out.println(lcs.findLCSLength("passport", "ppsspt"));
     }
 }
