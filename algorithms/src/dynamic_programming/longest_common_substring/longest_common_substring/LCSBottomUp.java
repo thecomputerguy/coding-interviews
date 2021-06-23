31a07670b06154d989c977a8be3d229a32bab3ee
Varun Sharma <varunsharma12@outlook.com>
2021-06-23 14:09:19 +0530

    😁 Fixed bug

--- algorithms/src/dynamic_programming/longest_common_substring/longest_common_substring/LCSBottomUp.java
+++ algorithms/src/dynamic_programming/longest_common_substring/longest_common_substring/LCSBottomUp.java
@@ -5,11 +5,11 @@ public class LCSBottomUp {
       return 0;
     }
 
-    int[][] dp = new int[s1.length()][s2.length()];
+    int[][] dp = new int[s1.length() + 1][s2.length() + 1];
     int maxLength = 0;
-    for (int i = 1; i < s1.length(); i++) {
-        for (int j = 1; j < s2.length(); j++) {
-            if(s1.charAt(i) == s2.charAt(j)){
+    for (int i = 1; i <= s1.length(); i++) {
+        for (int j = 1; j <= s2.length(); j++) {
+            if(s1.charAt(i-1) == s2.charAt(j-1)){
                 dp[i][j] = 1 + dp[i - 1][j - 1];
                 maxLength = Math.max(dp[i][j], maxLength);
             }            
